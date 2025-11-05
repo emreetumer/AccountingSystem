8bfcfafdfea0123151ea1c306187d26998c07b39
Emre Tümer <emreetumer0@gmail.com>
2025-11-05 17:00:25 +0300

    Invoice service created

--- AccountingSystem/AccountingSystem.WEBAPI/Entities/Product.cs
+++ AccountingSystem/AccountingSystem.WEBAPI/Entities/Product.cs
@@ -6,7 +6,7 @@ public sealed class Product : BaseEntity
 {
     public string Name { get; set; } = default!;
     public decimal UnitPrice { get; set; } = default!;
-    public string Unit { get; set; } = default!;
+    public string Unit { get; set; } = default!; // Adet, Kg, Litre
     public bool IsActive { get; set; } = true;
 
     public List<InvoiceItem> InvoiceItems { get; set; } = new();
