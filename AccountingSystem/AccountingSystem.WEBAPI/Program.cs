cfe90965027da0752983cde360a34c47f44a160c
Emre Tümer <emreetumer0@gmail.com>
2025-11-05 14:00:16 +0300

    Database Created

--- AccountingSystem/AccountingSystem.WEBAPI/Program.cs
+++ AccountingSystem/AccountingSystem.WEBAPI/Program.cs
@@ -1,6 +1,15 @@
+using AccountingSystem.WEBAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+
 
 var app = builder.Build();
 
